44e514c4308ee78062447eeeb8685ea0c27a0fa1
R4yL <luca.ray@protonmail.ch>
2026-01-01 13:20:03 +0100

    update API version from 4.12.0 to 4.16.0

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,7 @@ import "net/http"
 var DefaultHeader = http.Header{
 	"User-Agent":   []string{"Mozilla/5.0 (iPhone; CPU OS 17_4.1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/17.4.1 Mobile/10A5355d Safari/8536.25"},
 	"Content-Type": []string{"application/json;charset=UTF-8"},
-	"version":      []string{"4.12.0"},
+	"version":      []string{"4.16.0"},
 	"product":      []string{"llu.ios"},
 }
 
